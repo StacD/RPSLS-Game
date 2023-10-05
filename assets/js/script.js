d2ec677840cdf16fa2954ef028cee25e7cf3f960
StacD <>
2023-10-05 23:19:45 +0000

    constants for scores div and buttons  added to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log("test")
+const pScore = 0;
+const cScore = 0;
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+
+const rockBtn = document.getElementById("r");
+const paperBtn = document.getElementById("p");
+const scissorsBtn = document.getElementById("sc");
+const lizardBtn = document.getElementById("l");
+const spockBtn = document.getElementById("sp");
+
+
+
