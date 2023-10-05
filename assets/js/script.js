153e4fba18d46b3c7db9e10fa8b437b254be7582
StacD <>
2023-10-05 23:41:47 +0000

    edited comments for better understanding of what piece of code is for

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,14 @@ const cScore = 0;
 const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
 
+//buttons
 const rockBtn = document.getElementById("r");
 const paperBtn = document.getElementById("p");
 const scissorsBtn = document.getElementById("sc");
 const lizardBtn = document.getElementById("l");
 const spockBtn = document.getElementById("sp");
 
+//function for start of game for player to pick their option
 function start() {
     rockBtn.addEventListener("click", function() {
         play("r")
@@ -30,7 +32,7 @@ function start() {
         play("sp")
     })
 }
-
+//function for computer choice to be randomly selected
 function getComputerChoice() {
     const choices = ["r", "p", "sc", "l", "sp"]
     const ranNum = Math.floor(Math.random() *5); //allows computer pick number between 0-5 ie 0,1,2,3,4
