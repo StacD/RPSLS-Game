77ce27ab6c3744f627f14237b2b5a2c4ab21c59a
StacD <>
2023-10-08 21:36:06 +0000

    initial readme.md commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
-var pScore = 0;
+var pScore = 0; document.getElementById("player-score");
 var cScore = 0;
-
-//const playerOptions = [rBtn]
-
- //pBtn, scBtn, lBtn, spBtn];
 const computerOptions = ["r", "p", "sc", "l", "sp"];
 
+var choices = ["rock", "paper", "scissors",];
+
+window.onload = function () {
+    for (let i = 0; i < 5; i++) {
+        // <img id="rock" src="rock.png">
+        let choice = document.createElement("img");
+        choice.id = choices[i];
+        choice.src = choices[i] + ".png";
+        choice.addEventListener("click", selectChoice);
+        document.getElementById("choices").append(choice);
+    }
+}
 //Declared buttons
 const rBtn = document.getElementById("r").innerHTML = "Rock";
 const pBtn = document.getElementById("p").innerHTML = "Paper";
@@ -13,15 +21,17 @@ const scBtn = document.getElementById("sc").innerHTML = "Scissors";
 const lBtn = document.getElementById("l").innerHTML = "Lizard";
 const spBtn = document.getElementById("sp").innerHTML = "Spock";
 
-//Player Selection 
+//Player Selection
+//generate computers choice using random.
+
 var playerChoice = "Rock";
     document.getElementById("r").innerHTML = button;
 
     function pR() {
         document.getElementById("player-choice").innerHTML = "you picked rock"
 
-    var computerselection = computerOptions [Math.floor(Math.random() * 5)];
-        document.getElementById("computer-choice").innerHTML = "Computer" + computerselection;
+    var computerSelection = computerOptions [Math.floor(Math.random() * 5)];
+        document.getElementById("computer-choice").innerHTML = "Computer" + computerSelection;
     }
 
 var playerChoice = "Paper";
@@ -64,7 +74,15 @@ var playerChoice = "Spock";
     document.getElementById("computer-choice").innerHTML = "Computer" + computerselection;
     }
 
-//generate computers choice using random.
-
-
-   
+// Score record
+function results() {
+    if (playerChoice == computerSelection) {
+        pScore += 1;
+        cScore += 1;
+    }
+            
+    
+    
+    document.getElementById("player-score").innerHTML = "Your Score:" + pScore;
+    document.getElementById("computer-score").innerHTML = "Computer Score:" + cScore;
+    }
