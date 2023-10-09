d74e7bbed7c74d9eda633dffc5438b2d56430365
StacD <>
2023-10-09 15:20:36 +0000

    improved html layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,45 +7,45 @@ var choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
 window.onload = function () {
     for (let i = 0; i < 5; i++) {
-        //Getting images as options.
+        // <img id="rock" src="rock.png">
         let choice = document.createElement("img");
         choice.id = choices[i];
         choice.src = choices[i] + ".png";
         choice.addEventListener("click", selectChoice);
         document.getElementById("choices").append(choice);
     }
-}
-//Function for game play.
+};
+
 function selectChoice() {
     player = this.id;
     document.getElementById("player-choice").src = player + ".png";
 
-//Code for systems random choice.
-    system = choices[Math.floor(Math.random() * 5)];
+    //random for oppponent
+    system = choices[Math.floor(Math.random() * 5)]; //0- .999999 * 3 = 0-2.99999
     document.getElementById("system-choice").src = system + ".png";
 
-//Code for checking results based on player choice and system random choice.
+    //check for winner
     if (player == system) {
-        playerScore +=0;
-        systemScore +=0;
+        playerScore += 0;
+        systemScore += 0;
     }
-    else 
+    else
         if (player == "rock") {
             if (system == "scissors") {
                 playerScore += 1;
             }
             else if (system == "paper") {
-                    systemScore += 1;
+                systemScore += 1;
             }
             else if (system == "lizard") {
                 playerScore += 1;
             }
-            else if (system == "spock") {
+            else if (system = "spock") {
                 systemScore += 1;
             }
         }
 
-        else 
+        else
             if (player == "paper") {
                 if (system == "scissors") {
                     systemScore += 1;
@@ -56,12 +56,13 @@ function selectChoice() {
                 else if (system == "lizard") {
                     systemScore += 1;
                 }
-                else if (system == "spock") {
+                else if (system = "spock") {
                     playerScore += 1;
                 }
+
             }
 
-            else 
+            else
                 if (player == "scissors") {
                     if (system == "spock") {
                         systemScore += 1;
@@ -72,10 +73,12 @@ function selectChoice() {
                     else if (system == "lizard") {
                         playerScore += 1;
                     }
-                    else if (system == "paper") {
+                    else if (system = "paper") {
                         playerScore += 1;
                     }
+
                 }
+
                 else
                     if (player == "lizard") {
                         if (system == "spock") {
@@ -87,27 +90,27 @@ function selectChoice() {
                         else if (system == "scissors") {
                             systemScore += 1;
                         }
-                        else if (system == "paper") {
+                        else if (system = "paper") {
                             playerScore += 1;
                         }
-                }            
-                else
-                    if (player == "spock") {
-                        if (system == "lizard") {
+                    }
+
+                    else
+                        if (player == "spock") {
+                            if (system == "lizard") {
                                 systemScore += 1;
                             }
-                        else if (system == "rock") {
+                            else if (system == "rock") {
                                 playerScore += 1;
                             }
-                        else if (system == "scissors") {
+                            else if (system == "scissors") {
                                 playerScore += 1;
                             }
-                        else if (system == "paper") {
+                            else if (system = "paper") {
                                 systemScore += 1;
                             }
                         }
 
-//Getting results of game to show.   
-    document.getElementById("player-score").innerHTML = playerScore;
-    document.getElementById("system-score").innerHTML = systemScore;
+    document.getElementById("player-score").innerText = playerScore;
+    document.getElementById("system-score").innerText = systemScore;
 }
