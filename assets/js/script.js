4169cd7bf084b53142fb8d1524c17647565e12e9
StacD <>
2023-10-09 10:18:53 +0000

    simplified js code to match simplified html with the choice section as will show a clearer look to user, images will be what prompts responses

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-var pScore = 0; document.getElementById("player-score");
-var cScore = 0;
-const computerOptions = ["r", "p", "sc", "l", "sp"];
+var player;
+var playerScore = 0;
+var computer;
+var computerScore = 0;
 
-var choices = ["rock", "paper", "scissors",];
+var choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
 window.onload = function () {
     for (let i = 0; i < 5; i++) {
-        // <img id="rock" src="rock.png">
+        //Getting images as options.
         let choice = document.createElement("img");
         choice.id = choices[i];
         choice.src = choices[i] + ".png";
@@ -14,75 +15,25 @@ window.onload = function () {
         document.getElementById("choices").append(choice);
     }
 }
-//Declared buttons
-const rBtn = document.getElementById("r").innerHTML = "Rock";
-const pBtn = document.getElementById("p").innerHTML = "Paper";
-const scBtn = document.getElementById("sc").innerHTML = "Scissors";
-const lBtn = document.getElementById("l").innerHTML = "Lizard";
-const spBtn = document.getElementById("sp").innerHTML = "Spock";
+//Function for game play.
+function selectChoice() {
+    player = this.id;
+    document.getElementById("player-choice").src = player + ".png";
 
-//Player Selection
-//generate computers choice using random.
+//Code for computers random choice.
+    computer = choices[Math.floor(Math.random() * 5)];
+    document.getElementById("computer-choice").src = computer + ".png";
 
-var playerChoice = "Rock";
-    document.getElementById("r").innerHTML = button;
-
-    function pR() {
-        document.getElementById("player-choice").innerHTML = "you picked rock"
-
-    var computerSelection = computerOptions [Math.floor(Math.random() * 5)];
-        document.getElementById("computer-choice").innerHTML = "Computer" + computerSelection;
+//Code for checking results based on player choice and computer random choice.
+    if (player == computer) {
+        playerScore +=1;
+        computerScore +=1;
     }
 
-var playerChoice = "Paper";
-    document.getElementById("p").innerHTML = button;
 
-    function pP() {
-    document.getElementById("player-choice").innerHTML = "you picked paper";
 
-    var computerselection = computerOptions[Math.floor(Math.random() * 5)];
-    document.getElementById("computer-choice").innerHTML = "Computer" + computerselection;
-    }
 
-var playerChoice = "Scissors";
-    document.getElementById("sc").innerHTML = button;
-
-    function pSc() {
-    document.getElementById("player-choice").innerHTML = "you picked scissors";
-
-    var computerselection = computerOptions[Math.floor(Math.random() * 5)];
-    document.getElementById("computer-choice").innerHTML = "Computer" + computerselection;
-    }
-
-var playerChoice = "Lizard";
-    document.getElementById("l").innerHTML = button;
-
-    function pL() {
-    document.getElementById("player-choice").innerHTML = "you picked lizard";
-
-    var computerselection = computerOptions[Math.floor(Math.random() * 5)];
-    document.getElementById("computer-choice").innerHTML = "Computer" + computerselection;
-    }
-
-var playerChoice = "Spock";
-    document.getElementById("sp").innerHTML = button;
-
-    function pSp() {
-    document.getElementById("player-choice").innerHTML = "you picked spock";
-
-    var computerselection = computerOptions[Math.floor(Math.random() * 5)];
-    document.getElementById("computer-choice").innerHTML = "Computer" + computerselection;
-    }
-
-// Score record
-function results() {
-    if (playerChoice == computerSelection) {
-        pScore += 1;
-        cScore += 1;
-    }
-            
-    
-    
-    document.getElementById("player-score").innerHTML = "Your Score:" + pScore;
-    document.getElementById("computer-score").innerHTML = "Computer Score:" + cScore;
-    }
+//Getting results of game to show.   
+document.getElementById("player-score").innerHTML = "Your Score:" + pScore;
+document.getElementById("computer-score").innerHTML = "Computer Score:" + cScore;
+}
