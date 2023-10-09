cfb39edbfce3afbda5163ccb8ac167bb71ec645d
StacD <>
2023-10-09 10:47:09 +0000

    finished the js for possible results

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,20 +30,48 @@ function selectChoice() {
         computerScore +=1;
     }
     else {
-        if(player == "rock") {
+        if (player == "rock") {
             if (computer == "scissors" || "lizard") {
                 playerScore += 1;
             }
             else if (computer == "paper" || "spock") {
-                computerScore += 1;
+                    computerScore += 1;
             }
         }
-    }
-
-
-
+        else if (player == "paper") {
+            if (computer == "rock" || "spock") {
+                playerScore += 1;
+            }
+            else if (computer == "scissors" || "lizard") {
+                    computerScore += 1;
+            }
+        }
+        else if (player == "scissors") {
+            if (computer == "paper" || "lizard") {
+                playerScore += 1;
+            }
+            else if (computer == "rock" || "spock") {
+                    computerScore += 1;
+            }
+        }    
+        else if (player == "lizard") {
+            if (computer == "paper" || "spock") {
+                playerScore += 1;
+            }
+            else if (computer == "rock" || "scissors") {
+                    computerScore += 1;        
+        }
+        else if (player == "spock") {
+            if (computer == "rock" || "scissors") {
+                playerScore += 1;
+            }
+            else if (computer == "paper" || "lizard") {
+                    computerScore += 1;
+            }
+        }
 
 //Getting results of game to show.   
-document.getElementById("player-score").innerHTML = "Your Score:" + pScore;
-document.getElementById("computer-score").innerHTML = "Computer Score:" + cScore;
+document.getElementById("player-score").innerHTML = playerScore;
+document.getElementById("computer-score").innerHTML = computerScore;
+}
 }
